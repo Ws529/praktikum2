4cae93cc48fd89098639200e36759e6f6738348b
Wawan <wawansuwandi529@gmail.com>
2023-10-19 13:35:56 +0700

    first commit

---
+++ lab2.py/Latihan3.py
@@ -0,0 +1,22 @@
+# Input nilai variable
+a = input("Masukkan nilai a: ")
+b = input("Masukkan nilai b: ")
+
+# Cetak nilai variable
+print("Variable a =", a)
+print("Variable b =", b)
+
+print("Hasil penggabungan {} & {} = {}".format(a, b, a + b))
+# Cetak hasil operasi kedua variable dengan string format
+# Anda harus mengonversi a dan b ke tipe data int sebelum melakukan operasi matematika
+a = int(a)
+b = int(b)
+print("Hasil penjumlahan {} & {} = {}".format(a, b, a + b))
+
+# Konversi nilai variable sudah dilakukan di atas, jadi tidak perlu dilakukan lagi
+# Hapus baris berikut
+# a = int(a)
+# b = int(b)
+
+# Perhatikan bahwa operasi pembagian akan menghasilkan float, bukan int
+print("Hasil penjualan {} / {} = {}".format(a, b, a / b))
